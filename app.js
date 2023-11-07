edd52e7945861132b03c7d44935057bdc3532624
suryaramesh555 <suryaram5566@gmail.com>
2023-11-07 13:12:04 +0530

    files added

---
+++ app.js
@@ -0,0 +1,9 @@
+function lightOFF(){
+    document.getElementById("image1").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("image2").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+
+}
+function lightON(){
+    document.getElementById("image1").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("image2").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+}
